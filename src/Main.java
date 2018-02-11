7d63e94fc40de3aa256719d6c7876b6096a5cf7c
my computer 3 <my computer 3@DESKTOP-F70IH8F>
2018-02-11 21:13:36 +0200

    first

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+	
+	 public static void main(String[] args){
+		 
+		 View v=new View();
+		 v.setVisible(true);
+		 v.setSize(400,500);
+		 Model m= new Model();
+	
+      Controller c = new Controller(v,m);
+   
+   
+	 }
+}
+
+
